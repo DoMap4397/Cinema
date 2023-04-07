25bff010c1a7236cbf346a2183be42ace73dba9b
dongta3110 <75368551+dongta3110@users.noreply.github.com>
2023-04-07 19:29:51 +0700

    Add files via upload

---
+++ CinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package fpt.edu.vn.Cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
